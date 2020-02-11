82be67467c8d66a977fa714a4b1475cb35bff1da
Ivan Wang <ivanhaifengwang@users.noreply.github.com>
2020-02-11 14:34:22 -0700

    commit as we go

---
+++ main.js
@@ -0,0 +1,60 @@
+let myArray = []
+
+// for (let i = 0; i<10; i++) {
+//     // console.log("hello")
+//     if (i!==2 && i!==3){
+//         myArray.push(i)
+//     }
+// }
+// for (let i = 0; i<10; i++) {
+//     // 1 is true, 0 is false, i%2 is the remainder of divding
+//     // so this is skipping every second number
+//     if (i%2){
+//         myArray.push(`This is number: ${i}`)
+//         // == "This is number: " + i
+//         // that way, it will highlight the variables
+//     }
+
+//     if ((i+1)%2){
+//         myArray.push(i)
+//     }
+// console.log(myArray)    
+
+
+    // switch(i) {
+    //     case a://if i==a, then run the following code
+    //         myArray.push(i)           
+    //         console.log(`This is even number: ${i}`);
+    //         break;
+        
+    //     default:
+    //         console.log("i")
+    // }
+
+
+    // let array = ['r', 'g', 'b']
+    // switch(array[i]) {
+    //     case 'r':         
+    //     console.log(`This is red`);
+    //     break;
+    //     case 'g':         
+    //     console.log(`This is green`);
+    //     break;
+    //     case 'b':         
+    //     console.log(`This is blue`);
+    //     break;
+    //     default:
+    //     console.log("i")
+    //     // switch statement is for SIMPLE MATCHING.
+    // }
+
+    // console.log(myArray)
+// }
+
+
+
+let myArr = []
+for (let i = 0; i<10; i++) {
+// for (let i = 0; i<10; i++) {
+    console.log("hello")
+}
--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,52 +1,53 @@
-function setup() {
-    createCanvas(400, 200)
-    background(200)
-  }
-
-function makeSquare() {
-}
-
-function draw() {
-  var x = 10
-  var count = 5
-  for (var i = 0; i < count; i++) {     
-    for (var i2 = 0; i2 < count; i2++){
-    rect(x, 10*i2, 10, 10)
-    }
-  x = x + 15
-  }
-}
-
-// var yOne = 20
-// var yTwo = 100
-// var yThree = 180
-// var speedOne = 3
-// var speedTwo = 3
-// var speedThree = 3
-
 // function setup() {
-//   createCanvas(400, 400)
-//   background(200)
+//     createCanvas(400, 200)
+//     background(200)
+//   }
+
+// function makeSquare() {
+//   for (var i = 0; i < count; i++) {     
+//     for (var i2 = 0; i2 < count; i2++){
+//     rect(x, 10*i2, 10, 10)
+//     }
+//   x = x + 10
+//   }
 // }
 
 // function draw() {
-//   background(200)
+//   var x = 10
+//   var count = 10
 
-//   yOne = yOne + speedOne
-//   if (yOne < 0 || yOne > height) {
-//     speedOne = speedOne * -1
-//   }
-//   rect(30, yOne, 20, 20)
-
-//   yTwo = yTwo + speedTwo
-//   if (yTwo < 0 || yTwo > height) {
-//     speedTwo = speedTwo * -1
-//   }
-//   rect(60, yTwo, 20, 20)
-
-//   yThree = yThree + speedThree
-//   if (yThree < 0 || yThree > height) {
-//     speedThree = speedThree * -1
-//   }
-//   rect(90, yThree, 20, 20)
 // }
+
+var yOne = 0
+var yTwo = 100
+var yThree = 200
+var speedOne = 3
+var speedTwo = 3
+var speedThree = 3
+
+function setup() {
+  createCanvas(400, 300)
+  background(200)
+}
+
+function draw() {
+  background(200)
+
+  yOne = yOne + speedOne
+  if (yOne < 0 || yOne > height) {
+    speedOne = speedOne * -1
+  }
+  rect(30, yOne, 20, 20)
+
+  yTwo = yTwo + speedTwo
+  if (yTwo < 0 || yTwo > height) {
+    speedTwo = speedTwo * -1
+  }
+  rect(60, yTwo, 20, 20)
+
+  yThree = yThree + speedThree
+  if (yThree < 0 || yThree > height) {
+    speedThree = speedThree * -1
+  }
+  rect(90, yThree, 20, 20)
+}
